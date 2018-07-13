9dd42813968f4d00183eb0522342a3d322172448
Dwina Solihin <dwinasolihin@gmail.com>
2018-07-12 19:47:39 -0700

    adding photos and additional functionality

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // get the element we want, attach listener
-let clickable = document.querySelector("img.clickable");
-clickable.addEventListener("click", clickCounter)
+let clickable = document.querySelector("img#clickable");
+clickable.addEventListener("click", clickCounter);
 
 // declare the counter variable
 let counter = 0;
-console.log("counter", counter)
+console.log("counter: ", counter);
 
 // click handler function 
 function clickCounter() {
@@ -12,17 +12,17 @@ function clickCounter() {
 	counter ++;
 
 	// output click count to container in DOM 
-	console.log("counter", counter)
+    if (counter > 0 && counter <= 10){
+	 console.log("counter: ", counter);
+    } else if (counter > 10 && counter < 20){
+      document.getElementById("clickable").src = "../dancing-squirrel.jpg";
+      console.log("counter: ", counter);
+    } else {
+      document.getElementById("clickable").src = "../cute-bunny.jpg";
+      console.log("counter: ", counter);
+    }
 	
 	// get the container, update its contents	
-	let container = document.querySelector("p#clickcount")
-	container.innerHTML = counter
+	let container = document.querySelector("p#clickcount");
+	container.innerHTML = "Counter: " + counter;
 }
-
-
-
-
-
-
-
-
